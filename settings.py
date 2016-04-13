937b39dac76059446a2cfe565a995ef853ee092c
Cobunny <malatanggm@gmail.com>
2016-04-12 19:38:37 -0700

    Initial Commit

---
+++ settings.py
@@ -0,0 +1,5 @@
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '130445939344-c8k8h50vd2cml92v36dijdl8c4nlqqd2.apps.googleusercontent.com'
+
+
